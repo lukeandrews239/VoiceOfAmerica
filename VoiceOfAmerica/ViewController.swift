34c49ece2c4be9b6c87c90d950be774187280687
Luke Andrews <andrews.lu@husky.neu.edu>
2019-10-09 21:47:28 -0400

    Initial Commit

---
+++ VoiceOfAmerica/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VoiceOfAmerica
+//
+//  Created by Luke Andrews on 10/9/19.
+//  Copyright © 2019 Luke Andrews. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ VoiceOfAmericaTests/VoiceOfAmericaTests.swift
@@ -0,0 +1,34 @@
+//
+//  VoiceOfAmericaTests.swift
+//  VoiceOfAmericaTests
+//
+//  Created by Luke Andrews on 10/9/19.
+//  Copyright © 2019 Luke Andrews. All rights reserved.
+//
+
+import XCTest
+@testable import VoiceOfAmerica
+
+class VoiceOfAmericaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
